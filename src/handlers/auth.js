daf3d436ad6b1e86c43eda905b61fa22750dcb0d
Kalyan Chinna Bathula <kalyancharlie@gmail.com>
2024-09-24 12:43:34 +0530

    Create auth handler

--- src/handlers/auth.js
+++ src/handlers/auth.js
@@ -1,7 +1,7 @@
-const user = require('../controllers/users')
+const auth = require('../controllers/auth')
 const handler = require('../lib/handler')
 
-module.exports = new Proxy(user, {
+module.exports = new Proxy(auth, {
   get: function (target, prop) {
     if (target[prop]) return async (event) => await handler(target[prop], event)
   },
