8f7254e515b61afcf16c4e73ae33cecec8cbae2b
Kalyan Chinna Bathula <kalyancharlie@gmail.com>
2024-09-28 16:59:04 +0530

    Update order payment model

--- src/model/order-payment-model.js
+++ src/model/order-payment-model.js
@@ -4,11 +4,12 @@ const Joi = require('joi')
 
 const orderPaymentSchema = Joi.object({
   paymentId: Joi.string(),
-  orderId: Joi.string(),
-  paymentMethod: Joi.string().optional(),
+  orderId: Joi.string().required(),
+  userId: Joi.string().required(),
+  paymentMethod: Joi.string().required(),
   paymentAmount: Joi.number().optional(),
   paymentDoneAt: Joi.date().optional(),
-  paymentStatus: Joi.string().optional(),
+  paymentStatus: Joi.string().required(),
 })
 
 module.exports = { TABLE_ORDER_PAYMENTS: 'order_payments' }
